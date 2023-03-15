abda14732387e2f79095dd2f0952a789d422a24c
Raul Chin Caamal <raul.chim@macropay.mx>
2023-03-15 09:29:23 -0600

    change appId

--- src/components/FacebookButton.tsx
+++ src/components/FacebookButton.tsx
@@ -12,7 +12,7 @@ const FacebookButton = ({ handleUserData, disabled }: IFacebookButtonProps) => {
   useEffect(() => {
     window.fbAsyncInit = function () {
       window.FB.init({
-        appId: "757844632732700",
+        appId: "254336876916958",
         cookie: true,
         xfbml: true,
         version: "v16.0",
